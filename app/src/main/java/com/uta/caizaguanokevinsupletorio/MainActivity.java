7e3df4f813f2b46499ecbecd192d1a551d7ccb46
kevho <kevincaizaguano2@gmail.com>
2022-02-16 18:40:36 -0500

    commit 18:40

--- app/src/main/java/com/uta/caizaguanokevinsupletorio/MainActivity.java
+++ app/src/main/java/com/uta/caizaguanokevinsupletorio/MainActivity.java
@@ -3,12 +3,28 @@ package com.uta.caizaguanokevinsupletorio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextUsuario, editTextContraseña;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextUsuario = findViewById(R.id.editTextTextusuario);
+        editTextContraseña = findViewById(R.id.editTextTextContraseña);
+
+    }
+
+    public  void logiarse (View view)
+    {
+        String usuario = editTextUsuario.getText().toString();
+        String contraseña = editTextContraseña.getText().toString();
+
+
     }
 }
