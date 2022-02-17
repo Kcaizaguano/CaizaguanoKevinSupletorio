1e654654212a7dcafc85ce5fb91e28e22a9f5738
kevho <kevincaizaguano2@gmail.com>
2022-02-16 21:14:37 -0500

    commit 21:15

--- app/src/main/java/com/uta/caizaguanokevinsupletorio/ListaTareasActivityKDCC.java
+++ app/src/main/java/com/uta/caizaguanokevinsupletorio/ListaTareasActivityKDCC.java
@@ -1,10 +1,13 @@
 package com.uta.caizaguanokevinsupletorio;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
@@ -19,10 +22,7 @@ public class ListaTareasActivityKDCC extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_tareas_kdcc);
-        tabLayout = findViewById(R.id.tabbussi);
-        viewPager = findViewById(R.id.viewpager);
-        tab1 = findViewById(R.id.tabpersonal);
-        tab2 = findViewById(R.id.tabbussi);
+
     }
 
     @Override
@@ -30,5 +30,16 @@ public class ListaTareasActivityKDCC extends AppCompatActivity {
 
         getMenuInflater().inflate(R.menu.menukdcc,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itemAgregar:
+                Intent intent = new Intent(this,RegistrarTareaActivityKDCC.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
