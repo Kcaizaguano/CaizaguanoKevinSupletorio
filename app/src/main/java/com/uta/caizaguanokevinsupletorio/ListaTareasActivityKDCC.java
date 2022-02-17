5b229bad4469962f4d0fbb5559a7f513b3a2cce3
kevho <kevincaizaguano2@gmail.com>
2022-02-16 21:02:58 -0500

    commit 21:00

--- app/src/main/java/com/uta/caizaguanokevinsupletorio/ListaTareasActivityKDCC.java
+++ app/src/main/java/com/uta/caizaguanokevinsupletorio/ListaTareasActivityKDCC.java
@@ -1,16 +1,28 @@
 package com.uta.caizaguanokevinsupletorio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.Menu;
 
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class ListaTareasActivityKDCC extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    TabItem tab1,tab2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_tareas_kdcc);
+        tabLayout = findViewById(R.id.tabbussi);
+        viewPager = findViewById(R.id.viewpager);
+        tab1 = findViewById(R.id.tabpersonal);
+        tab2 = findViewById(R.id.tabbussi);
     }
 
     @Override
