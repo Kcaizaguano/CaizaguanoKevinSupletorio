2db6758146e32a7d26f9f9565915199f4a5bff25
kevho <kevincaizaguano2@gmail.com>
2022-02-16 19:14:09 -0500

    commit 19:15

--- app/src/main/java/com/uta/caizaguanokevinsupletorio/MainActivity.java
+++ app/src/main/java/com/uta/caizaguanokevinsupletorio/MainActivity.java
@@ -3,10 +3,12 @@ package com.uta.caizaguanokevinsupletorio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.uta.caizaguanokevinsupletorio.Datos.UsuariosHelperKDCC;
 
@@ -29,15 +31,42 @@ public class MainActivity extends AppCompatActivity {
         String usuario = editTextUsuario.getText().toString();
         String contraseña = editTextContraseña.getText().toString();
 
+        UsuariosHelperKDCC usuarioHelper = new UsuariosHelperKDCC(this,"usuariosDB",null,1);
+        SQLiteDatabase sql = usuarioHelper.getReadableDatabase();
+
+        String consulta = "SELECT Cedula,Contrasenia FROM Usuarios  WHERE Cedula=" + usuario;
+        Cursor cursor = sql.rawQuery(consulta,null);
+        if ( cursor.moveToFirst()){
+
+
+
+        }else{
+            Toast.makeText(this, "No se  encotraron registros",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        sql.close();
+
     }
 
     public  void registrar(View view)
     {
-        UsuariosHelperKDCC  usuariohelper = new UsuariosHelperKDCC(this, "usuariosDB",null,1);
-        SQLiteDatabase sql = usuariohelper.getWritableDatabase();
-        ContentValues lstvalores = new ContentValues();
+        try {
+            UsuariosHelperKDCC  usuariohelper = new UsuariosHelperKDCC(this, "usuariosDB",null,1);
+            SQLiteDatabase sql = usuariohelper.getWritableDatabase();
+            ContentValues lstvalores = new ContentValues();
+            lstvalores.put("Cedula","1805349246");
+            lstvalores.put("Contrasenia","Kevin123@");
+            lstvalores.put("Nombre","Kevin Caizaguano");
+            sql.insert("Usuarios", null,lstvalores);
+            sql.close();
+
+            Toast.makeText(this,"Insertado con exito", Toast.LENGTH_SHORT).show();
 
 
+        }catch (Exception ex){
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
