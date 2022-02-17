8d6c87044885685f80698153d1328929625574dc
kevho <kevincaizaguano2@gmail.com>
2022-02-16 20:00:35 -0500

    commit 20:00

--- app/src/main/java/com/uta/caizaguanokevinsupletorio/RegistroActivityKDCC.java
+++ app/src/main/java/com/uta/caizaguanokevinsupletorio/RegistroActivityKDCC.java
@@ -107,34 +107,42 @@ public class RegistroActivityKDCC extends AppCompatActivity {
     }
 
 
-    private boolean validarCedula(String document) {
-        byte sum = 0;
+    public boolean validarCedula(String cedula) {
+        boolean cedulaCorrecta = false;
+
         try {
-            if (document.trim().length() != 10)
-                return false;
-            String[] data = document.split("");
-            byte verifier = Byte.parseByte(data[0] + data[1]);
-            if (verifier < 1 || verifier > 24)
-                return false;
-            byte[] digits = new byte[data.length];
-            for (byte i = 0; i < digits.length; i++)
-                digits[i] = Byte.parseByte(data[i]);
-            if (digits[2] > 6)
-                return false;
-            for (byte i = 0; i < digits.length - 1; i++) {
-                if (i % 2 == 0) {
-                    verifier = (byte) (digits[i] * 2);
-                    if (verifier > 9)
-                        verifier = (byte) (verifier - 9);
-                } else
-                    verifier = (byte) (digits[i] * 1);
-                sum = (byte) (sum + verifier);
+
+            if (cedula.length() == 10) {
+                int tercerDigito = Integer.parseInt(cedula.substring(2, 3));
+                if (tercerDigito < 6) {
+                    int[] coefValCedula = {2, 1, 2, 1, 2, 1, 2, 1, 2};
+                    int verificador = Integer.parseInt(cedula.substring(9, 10));
+                    int suma = 0;
+                    int digito = 0;
+                    for (int i = 0; i < (cedula.length() - 1); i++) {
+                        digito = Integer.parseInt(cedula.substring(i, i + 1)) * coefValCedula[i];
+                        suma += ((digito % 10) + (digito / 10));
+                    }
+
+                    if ((suma % 10 == 0) && (suma % 10 == verificador)) {
+                        cedulaCorrecta = true;
+                    } else if ((10 - (suma % 10)) == verificador) {
+                        cedulaCorrecta = true;
+                    } else {
+                        cedulaCorrecta = false;
+                    }
+                } else {
+                    cedulaCorrecta = false;
+                }
+            } else {
+                cedulaCorrecta = false;
             }
-            if ((sum - (sum % 10) + 10 - sum) == digits[9])
-                return true;
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (NumberFormatException nfe) {
+            cedulaCorrecta = false;
+        } catch (Exception err) {
+            cedulaCorrecta = false;
         }
-        return false;
+        return  cedulaCorrecta;
     }
+
 }
