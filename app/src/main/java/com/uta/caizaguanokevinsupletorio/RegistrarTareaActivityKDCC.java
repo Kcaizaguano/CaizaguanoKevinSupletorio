6fbacc92f678d6504924c86c6f3b6fc6bd3fe45a
kevho <kevincaizaguano2@gmail.com>
2022-02-16 21:30:41 -0500

    commit 21:30

--- app/src/main/java/com/uta/caizaguanokevinsupletorio/RegistrarTareaActivityKDCC.java
+++ app/src/main/java/com/uta/caizaguanokevinsupletorio/RegistrarTareaActivityKDCC.java
@@ -3,6 +3,7 @@ package com.uta.caizaguanokevinsupletorio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class RegistrarTareaActivityKDCC extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class RegistrarTareaActivityKDCC extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_tarea_kdcc);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menuregistro,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
